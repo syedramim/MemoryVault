d9e7c473e93f242bd77b3f21b85f8f02e2b75b27
Kaneis Zontanos <ramim@bc.edu>
2025-02-02 16:29:23 -0500

    initial message

---
+++ MemoryVault/MemoryVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryVaultApp.swift
+//  MemoryVault
+//
+//  Created by Kaneis Zontanos on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
